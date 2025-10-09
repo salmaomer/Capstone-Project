b45c5466d76b566c2d2ef25ee400e0adb480244f
salma <salma4567omer@gmail.com>
2025-10-09 15:46:56 +0300

    Add URL routing for BookVault CRUD views

--- bookvault/urls.py
+++ bookvault/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('my-collection/', views.my_collection, name='my_collection'),
+    path('book/<int:pk>/', views.book_detail, name='book_detail'),
+    path('book/add/', views.book_create, name='book_add'),
+    path('book/<int:pk>/edit/', views.book_edit, name='book_edit'),
+    path('book/<int:pk>/delete/', views.book_delete, name='book_delete'),
 ]
