da0bcbdf22ac89ea6ae160380284f0168c75685b
salma <salma4567omer@gmail.com>
2025-10-09 15:52:06 +0300

    Implement CRUD views for BookVault (home, detail, add, edit, delete)

--- bookvault/views.py
+++ bookvault/views.py
@@ -1,5 +1,50 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Book
+from .forms import BookForm
 
 def home(request):
-    return HttpResponse("Welcome to BookVault 📚")
+    books = Book.objects.all().order_by('-created_at')
+    return render(request, 'home.html', {'books': books})
+
+@login_required
+def my_collection(request):
+    books = Book.objects.filter(user=request.user)
+    return render(request, 'my_collection.html', {'books': books})
+
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'book_detail.html', {'book': book})
+
+@login_required
+def book_create(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            book = form.save(commit=False)
+            book.user = request.user
+            book.save()
+            return redirect('my_collection')
+    else:
+        form = BookForm()
+    return render(request, 'book_form.html', {'form': form})
+
+@login_required
+def book_edit(request, pk):
+    book = get_object_or_404(Book, pk=pk, user=request.user)
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('my_collection')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'book_form.html', {'form': form})
+
+@login_required
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk, user=request.user)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('my_collection')
+    return render(request, 'book_confirm_delete.html', {'book': book})
