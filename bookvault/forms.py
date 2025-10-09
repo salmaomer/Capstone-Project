18f830c9c53091f2b766b412f221a8445e0374d2
salma <salma4567omer@gmail.com>
2025-10-09 16:39:33 +0300

    Add RegisterForm alongside BookForm in forms.py for user registration

--- bookvault/forms.py
+++ bookvault/forms.py
@@ -1,7 +1,18 @@
 from django import forms
 from .models import Book
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
+# ==== BookForm ====
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'price', 'description', 'cover_image']
+
+# ==== RegisterForm ====
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
