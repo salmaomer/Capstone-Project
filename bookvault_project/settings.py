b23b8ddf3d087abc7ef41f79084dbdfb4b6f739e
salma <salma4567omer@gmail.com>
2025-10-13 19:05:49 +0300

    Change database

--- bookvault_project/settings.py
+++ bookvault_project/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'bookvault_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bookvault_db',     
+        'USER': 'postgres',          
+        'PASSWORD': '0000', 
+        'HOST': 'localhost',         
+        'PORT': '5433',             
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
